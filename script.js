340621f26bf821ff67e3f9db6c0a214603d17063
gabrielalove <157277711+gabrielalove@users.noreply.github.com>
2024-02-04 10:52:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let -- variabila rescriptibila
+var -- versiune mai veche "let"
+const -- variabila constanta
+
+let name = "Name"; 1232434; // Number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
++ - * / % ** // math operation
+
+
+
+
+let value = 12.0;
+
+// = atribuim
+// == verificam/comparam valorile sunt la fel
+// === compara si tipul
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for (element => element.age); // for each
+let count = 0;
+while (count < 10){
+    console.log(count);
+    count = count + 1;
+    // count += 1
+}
+function nameFunc(p1,p2,p3){
+    //code
+}
+
+class className{
+    constructor(p1,p2){
+        this.p1 = p1;
+        this.p2 = p2;
